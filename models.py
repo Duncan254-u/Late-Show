0453833554b782f390eb494a5caeaf333a945fd5
Duncan Kibet <kibet2704@gmail.com>
2025-06-30 19:47:03 +0300

    episode attributes

--- models.py
+++ models.py
@@ -12,3 +12,9 @@ class Episode(db.model, SerializaterMixin):
     __tablename__ = 'episodes'
 
     id = db.Column(db.Integer, primary_key=True)
+
+    date = db.Column(db.String(20), nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    appearances = db.relationship('Appearance', backref='episode', cascade='all, delete-orphan')
+
